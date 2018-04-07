cfa76113792f84ecc51f1164645e264487f44001
Faisán <pepo@MacBook-Pro-de-Pepo.local>
2018-04-07 18:30:47 +0200

    Formulario para acceder terminado. Crea la parte de Crear un usuario y de mostrar la modal para cuando se clique sobre Olvidé la contraseña.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    pug = require('gulp-pug');
+
+let challenge = 'dailyui1';
+
+// PUG
+gulp.task('pug', ()=> {
+    gulp.src(`./dev/pug/${challenge}.pug`)
+        .pipe(pug())
+        .pipe(gulp.dest('./dist/'));
+});
+
+// SASS
+gulp.task('sass', ()=>{
+    gulp.src(`./dev/scss/${challenge}.scss`)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            versions: 'last 2 browsers'
+        }))
+        .pipe(gulp.dest('./dist/css/'));
+});
